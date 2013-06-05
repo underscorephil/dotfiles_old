0859ff9dd3678b8438277ceb1db1d9589afc835e
Javier Gonzalez <javigon.napster@gmail.com>
2013-06-05 20:00:45 +0200

    Make ls not showing hidden files

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -20,8 +20,8 @@ alias cdb='cd -'
 
 # Show human friendly numbers and colors
 alias df='df -h'
-alias ll='ls -alGh'
-alias ls='ls -Gh'
+alias ll='ls -lGh'
+#alias ls='ls -Gh'
 alias du='du -h -d 2'
 
 # show me files matching "ls grep"
