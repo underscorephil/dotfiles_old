7ba35a7367e570f4ce710f27008e7aa5c2ee945e
Javier González <javier@javigon.com>
2015-02-11 14:45:22 +0100

    Fix colors through ssh in iterm2

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -1,2 +1,4 @@
+LS_COLORS="di=00;34:ln=00;35:so=00;32:pi=01;33:ex=00;31:bd=00;34"
 export GREP_COLOR='1;33'
-alias ls='ls --color=auto'
+# [ "$TERM" = "xterm" ] && TERM="xterm-256color"
+alias ls='ls -FHG'
