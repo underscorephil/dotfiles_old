f8fbcfd3dba8e3b565ef2a86a33a631208c07416
Javier González <javier@javigon.com>
2015-02-11 15:23:51 +0100

    Correct color ssh in mac and linux

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -1,4 +1,6 @@
 LS_COLORS="di=00;34:ln=00;35:so=00;32:pi=01;33:ex=00;31:bd=00;34"
-export GREP_COLOR='1;33'
-# [ "$TERM" = "xterm" ] && TERM="xterm-256color"
-alias ls='ls --color=auto'
+if [[ "$(uname)" == "Darwin" ]]; then
+	alias ls='ls -FHG'
+elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
+	alias ls='ls --color=auto'
+fi
