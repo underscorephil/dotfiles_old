990d69e0197d5b52fd64e66e2271d8d01944e32a
Phil Jackson <github@underscorephil.com>
2015-08-11 00:56:03 -0500

    removing unused alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -154,11 +154,6 @@ alias srdm='spring rake db:migrate'
 alias srdt='spring rake db:migrate'
 alias srdmt='spring rake db:migrate db:test:prepare'
 
-# Sprintly - https://github.com/nextbigsoundinc/Sprintly-GitHub
-alias sp='sprintly'
-# spb = sprintly branch - create a branch automatically based on the bug you're working on
-alias spb="git checkout -b \`sp | tail -2 | grep '#' | sed 's/^ //' | sed 's/[^A-Za-z0-9 ]//g' | sed 's/ /-/g' | cut -d"-" -f1,2,3,4,5\`"
-
 # Phil
 
 alias http="http --pretty=all"
